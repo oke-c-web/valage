30b5113185b8b1aa1ba5d109d81c798c6f12686a
oke-c-web <aguval007@gmail.com>
2026-04-05 23:22:02 +0100

    Refactor main function and add utility functions

--- introduction.go
+++ introduction.go
@@ -1,11 +1,41 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-func main() {
-	for i:='a'; i <= 'z'; i++ {
+func PrintAlphabet() {
+	for i := 'a'; i <= 'z'; i++ {
 		fmt.Printf("%c", i)
 	}
 	fmt.Println()
+}
+
+func PrintReverseaAlphabet() {
+	for i := 'z'; i >= 'a'; i-- {
+		fmt.Printf("%c", i)
+	}
+	fmt.Println()
+}
+func PrintDigits() {
+	for i := 0; i <= 9; i++ {
+		fmt.Println(i)
+	}
+}
+func IsNegative(n int) string {
+	if n < 0 {
+		return "T"
+
+	}
+	return "F"
+}
+
+func main() {
+	PrintAlphabet()
+	PrintReverseaAlphabet()
+	PrintDigits()
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
 
